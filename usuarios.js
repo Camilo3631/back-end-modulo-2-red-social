f8689428153c2da32b7ab7008bebe45cd8a2d2d1
Camilo Acosta <camiloacosta1193@outlook.com>
2026-05-27 17:39:51 -0500

    endpotis de eliminar-cuenta y modificar cuenta

--- usuarios.js
+++ usuarios.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+// propiesta
+import { ObjectId } from "mongodb";
 
 const router = Router();
 
@@ -11,15 +13,61 @@ const router = Router();
 
 
 
+// Modificar cuenta 
+router.put('/modificar-cuenta', async (req, res ) => {
+    try {
+      
+        const userId = req.user.id;
+
+        const { nombre, apellido, usuario, contraseña} = req.body;
 
 
+        // Validamos un campo
+        if (!nombre &&  !apellido && !usuario && !contraseña) {
+            return res.status(400).json({
+                message: 'Debes enviar un campo al menos para ser actualizado'
+            });
+        }
+
+        // Se colocan los datos actualizados 
+        const datosActualizados = {};
+ 
+        if (nombre) datosActualizados.nombre = nombre;
+        if (apellido) datosActualizados.apellido = apellido;
+        if (usuario) datosActualizados.usuario= usuario;
+        if (contraseña) datosActualizados.contraseña = contraseña;
+
+        const result = await req.app.db.collection('usuario').updateOne(
+            { _id: new ObjectId(userId) },
+            { $set: datosActualizados }
+        );
+
+        res.status(200).json({
+           mensaje: "Ha sido actualizada la cuenta correctamente",
+           result
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            error: 'Error al actualizar la cuenta',
+        })
+    }
+});
 
 
+// Eliminar cuenta
+router.delete('/eliminar-cuenta', async (req, res) => {
+    try {
+        const eliminar = await req.app.db.collection('usuario').deleteOne(
+            { _id: new ObjectId(req.user.id)}   
+        );
 
-
-
-
-
+        res.json({ message: 'Cuenta eliminada', eliminar});
+     } catch (error) {
+        res.status(500).json({ error: 'Error al eliminar la cuenta' });
+     }
+    
+});
 
 
 
