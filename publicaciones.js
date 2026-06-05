10b4c7ff643f2f6773d136731ea320657c832b61
karlaperezn <karla.perezn96@alumnostrazos.net>
2026-06-05 15:06:13 +0200

    cambio en endpoints de publicacion

--- publicaciones.js
+++ publicaciones.js
@@ -5,10 +5,12 @@ const router = Router();
 //Mostrar publicaciones del usuario / Pantalla de perfil
 router.get("/:username", async (req, res) => {
   const username = req.params.username;
+
   const publicaciones = await req.app.locals.db
     .collection("publicaciones")
-    .find()
+    .find(username)
     .toArray();
+
   res.send({ data: publicaciones });
 });
 
@@ -23,10 +25,12 @@ router.get("/todas", async (req, res) => {
 
 //Crear una publicación
 router.post("/crear-publicacion", async (req, res) => {
+  //El front debe mandar: username y texto (y si quieres la fecha)
   const nuevaPublicacion = req.body;
+
   const publicacionCreada = await req.app.locals.db
     .collection("publicaciones")
-    .insertOne(nuevaPublicacion);
+    .insertOne({username: nuevaPublicacion.username, texto: nuevaPublicacion.texto});
 
   res.send({ data: publicacionCreada });
 });
@@ -36,6 +40,7 @@ router.put("/editar-publicacion", async (req, res) => {
     const usuario = req.body.usuario;
     const fecha = req.body.fecha;
     const nuevoTexto = req.body.texto;
+
   const publicacionEditada =
     await req.app.locals.db.collection("publicaciones").updateOne({usuario}, {$set: {texto: nuevoTexto, fecha: fecha}})
 
