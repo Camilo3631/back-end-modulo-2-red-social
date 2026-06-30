0acd7706d600c77d0eaf364d15040fc7b7e7a5d4
hugo <hugomdmg@gmail.com>
2026-06-30 11:23:12 +0200

    config cors

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ import chat from "./chat.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -42,7 +48,7 @@ async function connectDB() {
   const client = await MongoClient.connect(url);
   console.log("prueba");
   app.locals.db = client.db("social");
-    // console.log(process.env.db)
+  // console.log(process.env.db)
 
 
 }
