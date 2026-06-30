8fed21d0ffd0763b20cffcc5a93504ba779899d7
hugo <hugomdmg@gmail.com>
2026-06-30 11:41:33 +0200

    clear index

--- index.js
+++ index.js
@@ -2,52 +2,46 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import cors from "cors";
 import dotenv from "dotenv";
-
-dotenv.config();
-
-//Importar archivos endpoints
 import usuarios from "./usuarios.js";
 import publicaciones from "./publicaciones.js";
 import contacto from "./contacto.js";
 import chat from "./chat.js";
 
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 3000
+
 
 app.use(cors({
-  origin: "*"
+  origin: true
 }));
 
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//Rutas de archivos
 app.use("/usuarios", usuarios);
 app.use("/publicaciones", publicaciones);
-app.use("/contacto", contacto);
+app.use("/contactos", contacto);
 app.use("/chat", chat);
-app.use('/usuarios', usuarios);
-app.use('/publicaciones', publicaciones);
-app.use('/contactos', contacto);
-app.use('/chat', chat);
 
 
-
-
-app.listen(process.env.PORT || 3000);
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Local server running in port: ${PORT}`);
+  });
+}
 
 await connectDB();
 
-//cambiar db en vercel: mongodb+srv://karlaperezn96_db_user:EnUnLugar...@cluster0.84ay04h.mongodb.net/?appName=Cluster0
-//en .env db = mongodb://admin:admin123@127.0.0.1:27017
+//vercel: db = mongodb+srv://general:general1@cluster0.84ay04h.mongodb.net/?appName=Cluster0
+//.env: db = mongodb://admin:admin123@127.0.0.1:27017
 async function connectDB() {
-  console.log(process.env.db);
   const url = process.env.db;
   const client = await MongoClient.connect(url);
-  console.log("prueba");
   app.locals.db = client.db("social");
-  // console.log(process.env.db)
-
+  console.log("mongo connected to", url);
 
 }
 
