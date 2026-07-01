27193cb284e6ada7925b0ae11e68caf743d1354a
Hugo <hugomdmg@profesorestrazos.net>
2026-07-01 17:24:50 +0200

    endpoint delete user

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ import usuarios from "./usuarios.js";
 import publicaciones from "./publicaciones.js";
 import contacto from "./contacto.js";
 import chat from "./chat.js";
+import dns from "dns"
 
 dotenv.config();
+dns.setServers(['1.1.1.1', '1.0.0.1']);
 
 const app = express();
 const PORT = process.env.PORT || 3000
@@ -36,12 +38,10 @@ await connectDB();
 //vercel: db = mongodb+srv://general:general1@cluster0.84ay04h.mongodb.net/?appName=Cluster0
 //.env: db = mongodb://admin:admin123@127.0.0.1:27017
 async function connectDB() {
-  // const url = process.env.db;
-  const url = "mongodb+srv://general:general1@cluster0.vefyiih.mongodb.net/?appName=Cluster0"
+  const url = process.env.db;
   const client = await MongoClient.connect(url);
   app.locals.db = client.db("social");
   console.log("mongo connected to", url);
-
 }
 
 export default app;
--- usuarios.js
+++ usuarios.js
@@ -1,23 +1,20 @@
 import { Router } from "express";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 // propiesta
 import { ObjectId } from "mongodb";
 
 const router = Router();
 
-
-
 router.get("/", async (req, res) => {
+  const usuarios = await req.app.locals.db
+    .collection("usuarios")
+    .find()
+    .toArray();
 
-const usuarios = await req.app.locals.db.collection("usuarios").find().toArray();
-
-res.send(usuarios)
-
-})
-
+  res.send(usuarios);
+});
 
 router.post("/registrar", async (req, res) => {
-
   const datosUsuario = req.body;
 
   const username = datosUsuario.username;
@@ -29,11 +26,8 @@ router.post("/registrar", async (req, res) => {
 
   // Campos obligatorios
   if (!username || !email || !contrasena) {
-
     mensaje = "Todos los campos son obligatorios.";
-
   } else {
-
     // Formato email
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -42,16 +36,10 @@ router.post("/registrar", async (req, res) => {
     }
 
     // Formato username
-    else if (
-      username.length < 3 ||
-      /[^a-zA-Z0-9_]/.test(username)
-    ) {
-
+    else if (username.length < 3 || /[^a-zA-Z0-9_]/.test(username)) {
       mensaje =
         "El nombre de usuario debe tener mínimo 3 caracteres y solo puede contener letras, números y guión bajo.";
-
     } else {
-
       // Buscar existencia
       const usernameExiste = await req.app.locals.db
         .collection("usuarios")
@@ -64,28 +52,22 @@ router.post("/registrar", async (req, res) => {
       if (usernameExiste && emailExiste) {
         mensaje =
           "Ese nombre de usuario está en uso y ya existe una cuenta con ese correo electrónico.";
-
       } else if (usernameExiste) {
         mensaje = "Ese nombre de usuario está en uso.";
-
       } else if (emailExiste) {
-        mensaje =
-          "Ya existe una cuenta con ese correo electrónico.";
-
+        mensaje = "Ya existe una cuenta con ese correo electrónico.";
       } else {
-
         // Cifrar contraseña
         const saltRounds = 12;
-        const contrasenaCifrada =
-          await bcrypt.hash(contrasena, saltRounds);
-          
+        const contrasenaCifrada = await bcrypt.hash(contrasena, saltRounds);
+
         //Registrar usuario
         nuevoUsuario = await req.app.locals.db
           .collection("usuarios")
           .insertOne({
             username,
             email,
-            contrasena: contrasenaCifrada
+            contrasena: contrasenaCifrada,
           });
 
         mensaje = "Usuario registrado";
@@ -93,11 +75,8 @@ router.post("/registrar", async (req, res) => {
     }
   }
 
-  res.send({data: nuevoUsuario, mensaje});
-
+  res.send({ data: nuevoUsuario, mensaje });
 });
-
-
 
 //Iniciar sesion
 router.post("/iniciar-sesion", async (req, res) => {
@@ -132,11 +111,6 @@ router.post("/iniciar-sesion", async (req, res) => {
   res.send({ estado, mensaje, usuarioVerificado });
 });
 
-
-
-
-
-
 // Te va mejor un get que un post para esto porque es una consulta no una creación.
 router.post("/buscar", async (req, res) => {
   const buscarUser = await req.app.locals.db
@@ -151,73 +125,55 @@ router.post("/buscar", async (req, res) => {
   res.send({ data: buscarUser });
 });
 
+// Modificar cuenta
+router.put("/modificar-cuenta/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
 
+    const { nombre, apellido, usuario, contraseña } = req.body;
 
-
-
-
-
-// Modificar cuenta 
-router.put('/modificar-cuenta/:id', async (req, res ) => {
-    try {
-      
-        const userId = req.params.id
-
-        const { nombre, apellido, usuario, contraseña} = req.body;
-
-
-        // Validamos un campo
-        if (!nombre &&  !apellido && !usuario && !contraseña) {
-            return res.status(400).json({
-                message: 'Debes enviar un campo al menos para ser actualizado'
-            });
-        }
-
-        // Se colocan los datos actualizados 
-        const datosActualizados = {};
- 
-        if (nombre) datosActualizados.nombre = nombre;
-        if (apellido) datosActualizados.apellido = apellido;
-        if (usuario) datosActualizados.usuario= usuario;
-        if (contraseña) datosActualizados.contraseña = contraseña;
-
-        const result = await req.app.locals.db.collection('usuario').updateOne(
-            { _id: new ObjectId(userId) },
-            { $set: datosActualizados }
-        );
-
-        res.status(200).json({
-           mensaje: "Ha sido actualizada la cuenta correctamente",
-           result
-        })
-
-    } catch (error) {
-        res.status(500).json({
-            error: 'Error al actualizar la cuenta',
-        })
+    // Validamos un campo
+    if (!nombre && !apellido && !usuario && !contraseña) {
+      return res.status(400).json({
+        message: "Debes enviar un campo al menos para ser actualizado",
+      });
     }
-});
 
+    // Se colocan los datos actualizados
+    const datosActualizados = {};
+
+    if (nombre) datosActualizados.nombre = nombre;
+    if (apellido) datosActualizados.apellido = apellido;
+    if (usuario) datosActualizados.usuario = usuario;
+    if (contraseña) datosActualizados.contraseña = contraseña;
+
+    const result = await req.app.locals.db
+      .collection("usuario")
+      .updateOne({ _id: new ObjectId(userId) }, { $set: datosActualizados });
+
+    res.status(200).json({
+      mensaje: "Ha sido actualizada la cuenta correctamente",
+      result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Error al actualizar la cuenta",
+    });
+  }
+});
 
 // Eliminar cuenta
-router.delete('/eliminar-cuenta/:id', async (req, res) => {
-    try {
+router.post("/eliminar-cuenta/:email", async (req, res) => {
+  try {
+    const email = req.params.email;
+    const eliminar = await req.app.locals.db
+      .collection("usuarios")
+      .deleteOne({ email: email });
 
-        const userId = req.params.id
-
-
-        const eliminar = await req.app.locals.db.collection('usuario').deleteOne(
-            {_id: new ObjectId(userId)}   
-        );
-
-        res.json({ message: 'Cuenta eliminada', eliminar});
-     } catch (error) {
-        res.status(500).json({ error: 'Error al eliminar la cuenta' });
-     }
-    
+    res.send({ message: "Cuenta eliminada", eliminar: eliminar });
+  } catch (error) {
+    res.status(500).json({ error: "Error al eliminar la cuenta" });
+  }
 });
-
-
-
 
 export default router;
